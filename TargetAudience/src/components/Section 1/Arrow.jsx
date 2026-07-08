44005dcfa023da4bba226c79b74a0fc703ea99b0
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-08 23:44:26 +0500

    Added Hero Text

--- TargetAudience/src/components/Section 1/Arrow.jsx
+++ TargetAudience/src/components/Section 1/Arrow.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Arrow = () => {
   return (
     <div>
-      
+       {/* <i class="ri-arrow-right-up-line"></i> */}
     </div>
   )
 }
--- TargetAudience/src/components/Section 1/HeroText.jsx
+++ TargetAudience/src/components/Section 1/HeroText.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const HeroText = () => {
   return (
     <div>
-      {/* <h1 className='text-6xl px-20 py-15 leading-tight' >Prospective <br /> Customer  <br />Segmentation</h1> */}
+      <h1 className='text-6xl px-20 py-10 leading-tight font-bold' >Prospective <br /> Customer  <br />Segmentation</h1>
+      <p className='text-xl w-[90%] px-20 leading-9 text-gray-500'>Depending on customer satisfaction and access to banking products, potential target audience, can be divided into three groups</p>
     </div>
   )
 }
