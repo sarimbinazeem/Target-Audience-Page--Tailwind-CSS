2f77e87926547831738fdd0dfd2e8cdfe4ada782
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-09 00:36:22 +0500

    Added styling for Right section

--- TargetAudience/src/App.jsx
+++ TargetAudience/src/App.jsx
@@ -11,19 +11,19 @@ const App = () => {
     },
     {
       img: "https://images.unsplash.com/photo-1623880840102-7df0a9f3545b?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      color: "royalblue",
+      color: "orange",
       intro: "Prime customers, that have access to bank credit and are not satisfied with the current product",
       tag:"Underserved"
     },
     {
       img: "https://images.unsplash.com/photo-1760351561005-718d0e766dac?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      color: "royalblue",
+      color: "black",
       intro: "Customers from near-prime and sub-prime segments with no access to bank credit",
       tag:"Underbanked"
     },
     {
       img: "https://images.unsplash.com/photo-1657727534676-cac1bb160d64?q=80&w=704&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      color: "royalblue",
+      color: "lightseagreen",
       intro: "Sub-prime customers that dont have access to bank credit and are not satisfied with the current product",
       tag:"Unsatisfied"
     },
--- TargetAudience/src/components/Section 1/RightCardContent.jsx
+++ TargetAudience/src/components/Section 1/RightCardContent.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const RightCardContent = (props) => {
   return (
     <div className='absolute top-0 left-0 h-full w-full p-8 flex flex-col justify-between'>
-        <h2 className='bg-white text-xl font-semibold rounded-full h-12 w-12 flex justify-center items-center'>{props.id}</h2>
+        <h2 className='bg-white text-xl font-semibold rounded-full h-15 w-15 flex justify-center items-center'>{props.id}</h2>
         <div>
-            <p>{props.intro}</p>
-            <div>
-                <button style={{backgroundColor: props.color}}>{props.tag}</button>
-                <button style={{backgroundColor: props.color}}><i className="ri-arrow-right-line"></i></button>
+            <p className='text-white  text-lg font-semibold mb-10 text-shadow-2xs leading-relaxed'> {props.intro}</p>
+            <div className='flex justify-between '>
+                <button style={{backgroundColor: props.color}} className='rounded-full px-8 py-2 cursor-pointer font-medium text-white' >{props.tag}</button>
+                <button style={{backgroundColor: props.color}} className='text-white rounded-full px-8 py-2 cursor-pointer'><i className="ri-arrow-right-line"></i></button>
                 
             </div>
         </div>
