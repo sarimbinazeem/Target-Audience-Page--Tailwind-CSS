7a0d3105de9c06ff11a9c3706646bacad29cf459
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-09 00:41:56 +0500

    Removed ScrollBar

--- TargetAudience/src/components/Section 1/RightContent.jsx
+++ TargetAudience/src/components/Section 1/RightContent.jsx
@@ -3,7 +3,7 @@ import RightCard from './RightCard'
 
 const RightContent = (props) => {
   return (
-    <div className=' h-full w-2/3 flex flex-nowrap overflow-x-auto rounded-4xl p-6 gap-10 '>
+    <div id="right" className=' h-full w-2/3 flex flex-nowrap overflow-x-auto rounded-4xl p-6 gap-10 '>
         {props.users.map((elem,index)=>
         {
             return <RightCard key={index} color={elem.color} id={index+1} img={elem.img} tag={elem.tag} intro={elem.intro}/>
